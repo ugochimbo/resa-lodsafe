4df7cd87824a44838699be63501ede2e0c4a6ffd
Ugochukwu Ejikeme <ugochimbo@ugochmbo.com>
2014-11-21 19:05:00 +0100

    Replace nTwitter module with 'twitter' module

--- annotator.js
+++ annotator.js
@@ -4,6 +4,7 @@ var twitter = require('twitter');
 var io = require('socket.io');
 var _ = require('underscore');
 var path = require('path');
+//var util = require('util');
 // MongoDB
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -28,15 +29,21 @@ var t = new twitter({
     access_token_key: config.get['twitter_access_token_key'],
     access_token_secret: config.get['twitter_access_token_secret']
 });
+
 var stop_streaming=0;
+
 var stop=function(){
     stop_streaming=1;
-}
+};
+
 var pause_streaming=0;
+
 var pause=function(){
     pause_streaming=1;
-}
+};
+
 var start=function(watchSymbols,sockets) {
+    console.log("About starting watch for : " + watchSymbols);
     stop_streaming=0;
     pause_streaming=0;
     var send_data=0;
@@ -44,11 +51,11 @@ var start=function(watchSymbols,sockets) {
         return 0;
     }
     watchList.search_for=watchSymbols;
-// //Tell the twitter API to filter on the watchSymbols
-    t.stream('statuses/filter', { track: watchSymbols }, function(stream) {
 
-        //We have a connection. Now watch the 'data' event for in coming tweets.
+    //Tell the twitter API to filter on the watchSymbols
+    t.stream('filter', {track: watchSymbols}, function(stream) {
         stream.on('data', function(tweet) {
+            //console.log(util.inspect(data));
             if(stop_streaming){
                 stopStreaming(stream,sockets);
             }
@@ -58,16 +65,16 @@ var start=function(watchSymbols,sockets) {
                 sockets.sockets.emit('pause',{});
             }
             send_data=1;
-            var tweet_text=tweet.text;
+            var tweet_text = tweet.text;
             //Make sure it was a valid tweet
-            /*if (tweet_text !== undefined) {
+            if (tweet_text !== undefined) {
 
                 spotlight.sendRequest(tweet_text,function(output){
                     console.log('*********************************');
                     console.log(tweet_text);
                     if(output.Resources !=undefined){
                         //store tweets on DB
-                         db.get('tweetscollection').insert({"tweet_id":tweet.id,"user_name":tweet.user.screen_name,"created_at":tweet.created_at,"place":tweet.place,"processed":output});
+                        db.get('tweetscollection').insert({"tweet_id":tweet.id,"user_name":tweet.user.screen_name,"created_at":tweet.created_at,"place":tweet.place,"processed":output});
                         //console.log(output);
                         _.each(output.Resources, function(v) {
                             //do not count search keywords
@@ -81,10 +88,10 @@ var start=function(watchSymbols,sockets) {
                                     watchList.symbols[v['@surfaceForm']].count++;
                                     //Increment total
                                     watchList.total++;
-									//limit for demo
-									if(Object.keys(watchList.symbols).length>400){
-										pause_streaming=1;
-									}
+                                    //limit for demo
+                                    if(Object.keys(watchList.symbols).length>400){
+                                        pause_streaming=1;
+                                    }
                                     //console.log(watchList);
                                 }
                                 tweet_text=tweet_text.replace(v['@surfaceForm'],'&nbsp;<span resource="'+v['@URI']+'" class="r_entity r_'+getEntityType(v['@types']).toLowerCase()+'" typeOf="'+v['@types']+'">'+v['@surfaceForm']+'</span>&nbsp;');
@@ -97,21 +104,22 @@ var start=function(watchSymbols,sockets) {
                         //watchList.current_tweet.date=tweet.created_at;
                     }
                 })
-            }*/
+            }
         });
         //add a threshold to stop service if we got more than 1000 tweets
         if(watchList.tweets_no>1000){
             stopStreaming(stream,sockets);
         }
     });
-//acts as a buffer to slow down emiting results
+
+    //acts as a buffer to slow down emiting results
     setInterval(function(){
         if(send_data){
             sockets.sockets.emit('data', watchList);
             watchList.recent_tweets=[];
             send_data=0;
         }
-    },1500)
+    },1500);
     /*
      //filter out results for scalability
      setInterval(function(){
@@ -121,13 +129,13 @@ var start=function(watchSymbols,sockets) {
      }
      },3000)
      */
-}
+};
 var emptyWatchList=function(){
     watchList.tweets_no=0;
     watchList.total=0;
     watchList.recent_tweets=[];
     watchList.symbols={}
-}
+};
 var stopStreaming=function(stream,sockets){
  //Reset collection
 	db.get('tweetscollection').drop();
@@ -136,7 +144,7 @@ var stopStreaming=function(stream,sockets){
     stream.destroy();
     sockets.sockets.emit('data', watchList);
     sockets.sockets.emit('stop',{});
-}
+};
 var getEntityType=function(types_str){
     if(types_str==''){
         return 'Misc';
@@ -147,14 +155,14 @@ var getEntityType=function(types_str){
         if(tmp[0]=='Schema'){
             out= tmp[1];
         }
-    })
+    });
     if(out){
         return out;
     }else{
         return 'Misc';
     }
 
-}
+};
 //TODO:delete low level nodes when we get a huge amount of links
 var removeWeakSymbols=function(watchList){
     var filtered_list={};
@@ -172,14 +180,14 @@ var removeWeakSymbols=function(watchList){
         watchList.symbols=filtered_list;
     }
     return removed_ones;
-}
+};
 module.exports = {
     start: start,
     stop: stop,
     pause: pause,
     watchList: watchList,
     emptyWatchList:emptyWatchList
-}
+};
 //Reset everything on a new day!
 //We don't want to keep data around from the previous day so reset everything.
 /*
