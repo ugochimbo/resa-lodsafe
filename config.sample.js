e30d9ca49c37f257eaf79bbacd844dd52e6346c5
Ugochukwu Ejikeme <ugochimbo@ugochmbo.com>
2014-11-22 01:34:15 +0100

    Refactor out spotlight port

--- config.sample.js
+++ config.sample.js
@@ -1,9 +1,10 @@
 //application config
 exports.get={           // <--- FILL ME IN
-    spotlight_host:'spotlight.sztaki.hu:2226',
+    spotlight_host:'spotlight.sztaki.hu',
+    spotlight_port: '2222',
     spotlight_path:'rest/annotate',
     twitter_consumer_key:'',
     twitter_consumer_secret:'',
     twitter_access_token_key:'',
     twitter_access_token_secret:''
-}
+};
