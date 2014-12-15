b088e06956ecc5e6321a562525da396e00acc549
Ugochukwu Ejikeme <ugochimbo@ugochmbo.com>
2014-12-15 07:15:13 +0100

    Fix bug in overridden method

--- lodsafe.js
+++ lodsafe.js
@@ -2,15 +2,17 @@ var util = require('util');
 var Resa = require('./resa.js');
 
 function Lodsafe() {
+
+    this.isValidTweet = function () {
+        console.log("**********In Lodsafe************");
+        return (this.tweet.text !== undefined && this.tweet.place !== null);
+
+    };
+
     Resa.call(this);
 }
 
+
 util.inherits(Lodsafe, Resa);
-
-this.isValidTweet = function () {
-    console.log("**********In Lodsafe************");
-    return (this.tweet.text !== undefined && this.tweet.place !== null);
-
-};
 
 module.exports = Lodsafe;
