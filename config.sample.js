cbaf23e1cc22bf3a7576017f30d4a38f687a6a01
Ugochukwu Ejikeme <ugochimbo@ugochmbo.com>
2014-12-15 01:22:19 +0100

    Remove unneeded spotlight endpoint

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,5 @@
 //application config
 exports.get={           // <--- FILL ME IN
-    spotlight_host:'spotlight.sztaki.hu',
-    spotlight_port: '2222',
-    spotlight_path:'rest/annotate',
     twitter_consumer_key:'',
     twitter_consumer_secret:'',
     twitter_access_token_key:'',
