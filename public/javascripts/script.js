283d8d8efb78114c37da56ab7f6edd9b2443b492
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2014-12-20 23:02:02 +0100

    Add extension property to data json object

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -275,7 +275,7 @@
         var socket2 = io.connect(window.location.hostname);
         var data = {
             keywords : terms.split(','),
-            ext      : 'resa'
+            ext      : 'lodsafe'
         };
         socket2.emit('startA', data);
     }
