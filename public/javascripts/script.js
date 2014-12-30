e27147360519285a9da8d545d43510e4249f1772
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2014-12-30 01:55:05 +0100

    Extract methods for updating page sections

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -194,24 +194,36 @@
         socket.on('data', function(data) {
             //console.log(data);
             //console.log(data.recent_tweets);
-            var one_node_already_inserted=0;
-            var total = data.total;
-			var symbols_no=Object.keys(data.symbols).length;
-			var max_ent=400;
-			if(symbols_no>max_ent){
-				pauseAnalyzing();
-				alert('The demo is limited to '+max_ent+' entities! contact us for more info: khalili@informatik.uni-leipzig.de');
-			}
 
-            var avg_no=total/symbols_no;
-            var slug_text='';
-            $('#symbols_no').html(symbols_no).addClass("animated bounceIn");
-            $('#tweets_no').html(data.tweets_no).addClass("animated bounceIn");
-            if(data.tweets_no>0 && !glob_paused){
-                establishPauseMode();
-            }else{
-                glob_paused=0;
-            }
+            var params = {
+                one_node_already_inserted: 0,
+                total: data.total,
+                symbols_no: Object.keys(data.symbols).length,
+                max_ent:  400
+            };
+
+            updateTopPanelInfo(data, params);
+
+            //Right Panel (Tweet Stream)
+            updateTwitterStream(data);
+
+            //Main Panel (Viz)
+            updateVisualization(data, params);
+
+            restart();
+
+            $('#last-update').text(new Date().toTimeString());
+        });
+        socket.on('stop', function(data) {
+            stopAnalyzing();
+        });
+        socket.on('pause', function(data) {
+            glob_paused=1;
+            pauseAnalyzing();
+        });
+
+        function updateTwitterStream(data)
+        {
             $('#hashtag').html(' (#'+data.search_for.join()+')').addClass("animated bounceIn");
             $('.tweet').removeClass('animated').removeClass('flash');
             $('.r_entity').css('background-color','');
@@ -223,12 +235,17 @@
                 var id=convertToSlug($(this).text());
                 d3.select("#bubblecloud svg").selectAll('#t_'+id).attr('opacity',0.9);
             }).mouseout(function(){
-                    $(this).css('background-color','');
-                    var id=convertToSlug($(this).text());
-                    d3.select("#bubblecloud svg").selectAll('#t_'+id).attr('opacity',0);
-                });
+                $(this).css('background-color','');
+                var id=convertToSlug($(this).text());
+                d3.select("#bubblecloud svg").selectAll('#t_'+id).attr('opacity',0);
+            });
+        }
+
+        function updateVisualization(data, params)
+        {
+            var slug_text = "";
             for (var key in data.symbols) {
-                var val = data.symbols[key].count / total;
+                var val = data.symbols[key].count / params.total;
                 if (isNaN(val)) {
                     val = 0;
                 }
@@ -237,34 +254,48 @@
                 if(!d3.select("#bubblecloud svg").selectAll('.node-circle[id="' + slug_text + '"]').size()){
                     var start_x=width/2;
                     var start_y=height/2;
-                    if(one_node_already_inserted>0){
+                    if(params.one_node_already_inserted>0){
                         //prevent collision
-                        start_y=start_y-(one_node_already_inserted*15);
+                        start_y=start_y-(params.one_node_already_inserted*15);
                     }
                     //var category=Math.floor(20*Math.random());
                     var c_size=rScale(data.symbols[key].count);
                     var uri=data.symbols[key].uri;
                     var node = {x: start_x, y:start_y, name:key,n_weight:data.symbols[key].count, category:data.symbols[key].type, r:c_size, proportion:val,slug_text:slug_text,uri:uri},
                         n = nodes.push(node);
-                    one_node_already_inserted++;
-                }else{
-                    var new_size=rScale(data.symbols[key].count);
-                    if(d3.select("#bubblecloud svg").select('.node-circle[id="' + slug_text + '"]').attr('r')!=new_size){
-                        d3.select("#bubblecloud svg").select('.node-circle[id="' + slug_text + '"]').attr('r',new_size/2).transition().duration(700).attr('r',new_size);
+                    params.one_node_already_inserted++;
+                }
+                else{
+                        var new_size=rScale(data.symbols[key].count);
+                        if(d3.select("#bubblecloud svg").select('.node-circle[id="' + slug_text + '"]').attr('r')!=new_size){
+                            d3.select("#bubblecloud svg").select('.node-circle[id="' + slug_text + '"]').attr('r',new_size/2).transition().duration(700).attr('r',new_size);
                     }
                 }
             }
-            restart();
-            $('#last-update').text(new Date().toTimeString());
-        });
-        socket.on('stop', function(data) {
-            stopAnalyzing();
-        });
-        socket.on('pause', function(data) {
-            glob_paused=1;
-            pauseAnalyzing();
-        });
+        }
+
+        function updateTopPanelInfo(data, params)
+        {
+
+            if(params.symbols_no > params.max_ent){
+                pauseAnalyzing();
+                alert('The demo is limited to '+max_ent+' entities! contact us for more info: khalili@informatik.uni-leipzig.de');
+            }
+
+            /*var avg_no = params.total / params.symbols_no;
+            var slug_text = '';*/
+            $('#symbols_no').html(params.symbols_no).addClass("animated bounceIn");
+            $('#tweets_no').html(data.tweets_no).addClass("animated bounceIn");
+            if(data.tweets_no>0 && !glob_paused){
+                establishPauseMode();
+            }else{
+                glob_paused=0;
+            }
+        }
     });
+
+
+
     function startAnalyzing(){
         glob_paused=0;
         var terms=$('#keyword').val();
