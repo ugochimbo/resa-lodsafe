10293e99849f3220020306fa6dc5abd8e557e515
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-04 04:59:04 +0100

    Add task to concat client extension files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,27 @@
  */
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
 
-gulp.task('default', function() {
+var dir = "public/javascripts/extensions/";
 
+var src = [
+            dir + 'resa.js',
+            dir + 'lodsafe.js',
+            dir + 'extensionfactory.js',
+            dir + 'extensions.js'
+        ];
+
+gulp.task('extensions', function () {
+     return gulp.src(src)
+                .pipe(concat('script.js'))
+                .pipe(gulp.dest(dir + 'dist/'))
+                .pipe(notify({ message: 'Finished Concatenating Extension Scripts'}));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(dir + '*.js', ['extensions']);
+});
+
+gulp.task('default', ['extensions', 'watch']);
