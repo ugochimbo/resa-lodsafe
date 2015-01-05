6752934ab889b8f22a78497aa5e0e4341d2b34c3
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-05 02:52:35 +0100

    Add initial lodsafe client extension

---
+++ public/javascripts/extensions/entities/lodsafe.js
@@ -0,0 +1,17 @@
+
+//"""""""""" LODSAFE """""""""""//
+var lodsafe = new Extension();
+
+lodsafe.params = {
+    'strict' : true
+};
+
+lodsafe.setParams = function (params) {
+  this.params.strict = params.strict;
+};
+
+lodsafe.visualizations = [
+    'bubblecloud'
+];
+//"""""""" /LODSAFE """"""""""""//
+
