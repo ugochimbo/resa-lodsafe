4950254ed4d9dfc201fdd2640d863be0a7bb46b9
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-06 10:01:29 +0100

    Initial extensions handler file

---
+++ public/javascripts/extensions/dist/exthandlers.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ugochimbo on 1/5/2015.
+ */
+
+var lodsafe_mode = $("#lodsafe-mode");
+
+function init() {
+    lodsafe_mode.bootstrapSwitch({
+        'state': true,
+        'animate': true,
+        'handleWidth': 80,
+        'onColor': 'success',
+        'offColor': 'danger'
+    });
+}
+
+lodsafe_mode.on('switchChange.bootstrapSwitch', function(event, state) {
+    console.log(state); // true | false
+});
+
+init();
