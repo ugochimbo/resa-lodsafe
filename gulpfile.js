a6ad0656fa29d52e913bc2dd2db303b81dc74753
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-16 14:26:14 +0100

    Update links

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,34 @@ var notify = require('gulp-notify');
 
 var extensionsDir = "public/javascripts/extensions";
 var handlersDir = extensionsDir + "/handlers/";
-var handlers = [
+
+var visualizationsDir = "public/javascripts/visualizations/";
+var visualizations = [
+                        visualizationsDir + 'visualizations.js',
+                        visualizationsDir + 'bubblecloud.js'
+                ];
+
+var extensionsHandler = [
                     handlersDir + 'resaHandler.js',
                     handlersDir + 'lodsafeHandler.js'
                ];
 
 gulp.task('extensionsHandler', function () {
-    return gulp.src(handlers)
+    return gulp.src(extensionsHandler)
         .pipe(concat('extensionsHandler.js'))
         .pipe(gulp.dest(extensionsDir + '/dist/'))
         .pipe(notify({ message: 'Finished Concatenating Extensions Handler'}));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['extensionsHandler']);
+gulp.task('visualizations', function () {
+    return gulp.src(visualizations)
+        .pipe(concat('visualizations.js'))
+        .pipe(gulp.dest(visualizationsDir + '/dist/'))
+        .pipe(notify({ message: 'Finished Concatenating Visualizations'}));
 });
 
-gulp.task('default', ['extensionsHandler', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch(['extensionsHandler', 'visualizations']);
+});
+
+gulp.task('default', ['extensionsHandler', 'visualizations', 'watch']);
