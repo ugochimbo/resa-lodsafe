4ae2ad94e67cddbaa593c23657c51300db25c2e6
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-16 14:11:28 +0100

    Initial visualizations superclass

---
+++ public/javascripts/visualizations/visualizations.js
@@ -0,0 +1,11 @@
+function Visualizations() {
+
+    this.svg = null;
+    this.width = 900;
+    this.height = 900;
+
+}
+
+Visualizations.prototype = {
+    updateVisualization: function (data) {}
+};
