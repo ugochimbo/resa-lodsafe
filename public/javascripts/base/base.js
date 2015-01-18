f6b7473efc6e91fb503bcb25fd8cda692e2d9a95
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-18 16:46:13 +0100

    Add initial base object

---
+++ public/javascripts/base/base.js
@@ -0,0 +1,57 @@
+function Base() {
+    var glob_paused=0;
+    var extParams = {};
+
+    this.getCurrentVisualizationName = function () {
+        /*if(extParams.param.length > 1)
+         return $( ".selector" ).tabs( "option", "active" ).attr( 'id' );
+         else
+         return extParams.param;*/
+
+        return "bubblecloud";
+    };
+
+    this.getCurrentVisualizationObject = function() {
+        var visualizationName = this.getCurrentVisualizationName();
+        var visualizationFactory = new VisualizationFactory();
+        return visualizationFactory.createVisualizationObject(visualizationName);
+    };
+
+    this.getGlobPaused = function () {
+        return glob_paused;
+    };
+
+    this.getExtensionParams = function() {
+        return extParams;
+    };
+
+    this.resetVisualization = function() {
+
+    };
+
+    var addVisualizationTab = function (visualizations) {
+
+        console.log(JSON.stringify(visualizations));
+
+        var tabAnchor = "";
+        var tabContent = "";
+        for (var index = 0; index < visualizations.length; ++index) {
+            tabAnchor += '<li> <a data-toggle="tab" href="#' + visualizations[index].name +'">' + visualizations[index].title +'</a></li>';
+            tabContent += '<div id="' + visualizations[index].name + '" class="tab-pane"></div>';
+        }
+
+        $('#visualizations').append(tabAnchor);
+        $('#content').append(tabContent);
+    };
+
+    var setActiveVisualizationTab = function (){
+        $('#visualizations').find('li').first().addClass("active");
+        $('#content').find('div').first().addClass("active");
+    };
+
+    this.initVisualizations = function(visualizations) {
+        addVisualizationTab(visualizations);
+        setActiveVisualizationTab();
+    };
+
+}
