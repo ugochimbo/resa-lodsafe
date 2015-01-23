2eeeff9678a16c41eba3943cdaa30e5b5cc96925
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-01-23 09:54:48 +0100

    Let appHandler handle generic app events

--- public/javascripts/app/domEvents.js
+++ public/javascripts/app/domEvents.js
@@ -1,6 +1,21 @@
 
 //************ DOM Events *************//
 
+//** TODO: Refactor view.. Unclean code **//
+
+function startAnalyzing(){
+    appHandler.startAnalyzing();
+}
+
+function pauseAnalyzing() {
+   appHandler.pauseAnalyzing();
+}
+
+function stopAnalyzing() {
+    appHandler.stopAnalyzing();
+}
+
+//** Extension Change **//
 $("#extensions-list").on( "change", function() {
     appHandler.handleExtensionChange();
 });
