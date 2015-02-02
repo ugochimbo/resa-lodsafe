c4bb87d3383db69516a122e0e2b6b5440fc4f4e6
Ugochukwu Ejikeme <ugochimbo@ugochimbo.com>
2015-02-02 03:40:41 +0100

    Extract onSocketData method

--- public/javascripts/app/socketEvents.js
+++ public/javascripts/app/socketEvents.js
@@ -4,26 +4,7 @@
 var socket = io.connect(window.location.hostname);
 
 socket.on('data', function(data) {
-
-    var params = {
-        total: data.total,
-        symbols_no: Object.keys(data.watchList.symbols).length,
-        max_ent:  400
-    };
-
-    appHandler.updateTopPanelInfo(data.watchList, params);
-
-    //Right Panel (Tweet Stream)
-    appHandler.updateTwitterStream(data.watchList);
-
-    if(appHandler.isInitData(data)) {
-        appHandler.initExtensionParams(data);
-    }
-
-    //Main Panel (Viz)
-    appHandler.updateVisualization(data.watchList, params);
-
-    $('#last-update').text(new Date().toTimeString());
+    appHandler.onSocketData(data);
 });
 
 socket.on('stop', function(data) {
