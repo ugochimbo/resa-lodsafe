05240f0a720ace01e08adef6e197157069b9afcc
Ugochukwu Ejikeme <me@ugochimbo.com>
2015-09-21 23:26:44 +0200

    remp add config

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ugochimbo on 5/1/2015.
+ */
+//application config
+exports.get={           // <--- FILL ME IN
+    spotlight_host:'spotlight.sztaki.hu:2226',
+    spotlight_path:'rest/annotate',
+    twitter_consumer_key:'VYO3NLdQdqBpSaZbIGX1neKPc',
+    twitter_consumer_secret:'mFXUHewQ6Tm6uqMcPcC5cULP4srJiHkinnHPHaU4sr8mnFWeNU',
+    twitter_access_token_key:'194394595-sRmfRReGK81OMavgnj4n51mg1dFuVXYutoic8r4O',
+    twitter_access_token_secret:'ISuHYloA6NJeRUlZVaxHNwjIMgRnuEoRxu9H9B6yqf0dj'
+};
